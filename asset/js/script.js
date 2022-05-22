38e5e351d170cebc59372645f0880646c2c4c7cb
mtzsanad <mtz.sanad@gmail.com>
2022-05-22 21:32:51 +0200

    adding content

---
+++ asset/js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  var typed = new Typed(".typing1", {
+    strings: [
+      "Full Stack Developer",
+      "Freelancer",
+      "Data Scientist",
+      "Product Manager",
+    ],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+  });
+});
